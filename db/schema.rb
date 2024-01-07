584f862d915be0ee41fb72464d7d41cf8d6df672
dsktnd <daisuke.tsunoda@gmail.com>
2024-01-07 09:12:07 +0900

    yamlファイルのアップデート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_04_143226) do
+ActiveRecord::Schema.define(version: 2024_01_06_064601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2024_01_04_143226) do
     t.string "name"
     t.integer "price"
     t.string "vendor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ps", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
